986eb9e9b039c98bf5277df20a3dd5bb2e14ed6c
Zubkov Mihail <zubkovmihail12345@gmail.com>
2022-10-18 12:39:13 +0300

    first commit

---
+++ server/apache/dynamic.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zubkov Mihail</title>
+</head>
+<body>
+    <?php
+        $variable = $_GET['num'];
+        echo "<h1>$variable</h1>";
+    ?>
+    <a href="http://localhost">Статическая страница</a>
+</body>
+</html>
